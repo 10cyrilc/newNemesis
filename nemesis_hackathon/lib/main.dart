7042a2a3dd055382409da0352569356a8d3e578d
10cyrilc <10cyrilc@gmail.com>
2022-11-06 06:17:33 +0530

    Auth

--- nemesis_hackathon/lib/main.dart
+++ nemesis_hackathon/lib/main.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:nemesis_hackathon/screens/home_page.dart';
 import 'package:nemesis_hackathon/screens/login_screen.dart';
 import 'package:nemesis_hackathon/widgets/bottomNavbar.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -18,11 +24,11 @@ class MyApp extends StatelessWidget {
               const ColorScheme.light(primary: Color.fromARGB(255, 34, 34, 34))
           // primarySwatch: Colors.blue,
           ),
-      home: const LoginScreen(),
+      home: LoginScreen(),
       routes: {
         '/home_page_organisation': ((context) => BottomNavBar()),
-        '/logout': ((context) => const LoginScreen()),
-        '/home': ((context) => const HomePage())
+        '/logout': ((context) => LoginScreen()),
+        '/home': ((context) => HomePage())
       },
     );
   }
